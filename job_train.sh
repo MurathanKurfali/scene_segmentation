72035b934e2c2080a35258e50990849acf10235a
Murathan <murathankurfali@gmail.com>
2021-06-14 12:15:19 +0200

    parameter updates

--- job_train.sh
+++ job_train.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 #SBATCH --gpus-per-node=V100:1
-#SBATCH -t 0-2:00:00
+#SBATCH -t 0-1:00:00
 
-#SBATCH --output=logs/train.out
+#SBATCH --output=logs/train-%j.out
 
 module load GCC/8.3.0  CUDA/10.1.243  OpenMPI/3.1.4
 module load PyTorch/1.6.0-Python-3.7.4
