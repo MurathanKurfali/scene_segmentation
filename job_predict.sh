680a685fb372230435dd38d3bd0c213b2cf51d38
murathan <murathankurfali@gmail.com>
2021-06-15 11:07:58 +0200

    pred update

---
+++ job_predict.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+#SBATCH --gpus-per-node=T4:1
+#SBATCH -t 0-01:00:00
+
+#SBATCH --output=logs/pred-%j.out
+
+module load GCC/8.3.0  CUDA/10.1.243  OpenMPI/3.1.4
+module load PyTorch/1.6.0-Python-3.7.4
+module load Python/3.7.4
+
+source /cephyr/users/murathan/Alvis/deep/bin/activate
+
+
+for d in "large_"*/ ; do
+    echo $d
+    ./scripts/predict.sh $d
+done
+
+
+for d in "base_"*/ ; do
+    echo $d
+    ./scripts/predict.sh $d
+done
