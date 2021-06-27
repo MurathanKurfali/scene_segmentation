83b8a9200545fe75baecb864a714ae3bb9f70b3c
Murathan <murathankurfali@gmail.com>
2021-06-27 20:58:14 +0200

    predict.py added

--- job_predict.sh
+++ job_predict.sh
@@ -10,14 +10,10 @@ module load Python/3.7.4
 
 source /cephyr/users/murathan/Alvis/deep/bin/activate
 
-
+python predict.py
+:"
 for d in "large_"*/ ; do
     echo $d
     ./scripts/predict.sh $d
 done
-
-
-for d in "base_"*/ ; do
-    echo $d
-    ./scripts/predict.sh $d
-done
+"
