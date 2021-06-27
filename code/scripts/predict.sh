bc9551c95dc9b019fb40e0ff12845288879185cf
Murathan <murathankurfali@gmail.com>
2021-06-27 21:19:06 +0200

    predict.py added

--- code/scripts/predict.sh
+++ code/scripts/predict.sh
@@ -4,4 +4,4 @@ model=$1
 test_file=$2
 output_file=$3
 output_file="predictions/${output_file}.pred"
-python -m allennlp predict  "${model}" "${test_file}" --output-file "${output_file}" --silence  --use-dataset-reader --predictor SeqClassificationPredictor --include-package code.sequential_sentence_classification --cuda-device 0
+python -m allennlp predict  "${model}" "${test_file}" --output-file "${output_file}" --silent  --use-dataset-reader --predictor SeqClassificationPredictor --include-package code.sequential_sentence_classification --cuda-device 0
