d84bac80bd9bcfe1751d6c1ead2c4e1875aea9ee
Murathan <murathankurfali@gmail.com>
2021-06-28 00:39:12 +0200

    sanity check

--- predict.py
+++ predict.py
@@ -23,6 +23,8 @@ if __name__ == "__main__":
     reset_folder(pred_folder)
 
     for test_file in test_files:
+        if "9783845397535" not in test_file:
+            continue
         test_file_path = "{}/{}".format(test_folder, test_file)
         tmp_file_path = "{}/{}".format(temp_folder, test_file + "l")
         predicted_file_path = "{}/{}".format(pred_folder, test_file + ".pred")
