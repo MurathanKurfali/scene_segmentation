b14d1581831d650e433aff9eca62654ccac5427a
Murathan <murathankurfali@gmail.com>
2021-06-27 23:08:12 +0200

    sanity check

--- sanity_check.py
+++ sanity_check.py
@@ -16,6 +16,7 @@ from sklearn.metrics import f1_score, classification_report, confusion_matrix
 
 logging.getLogger().setLevel(logging.DEBUG)
 
+
 def read_jsonlines(file_path):
     content = []
     with jsonlines.open(file_path) as f:
@@ -23,9 +24,8 @@ def read_jsonlines(file_path):
             content.append(line)
     return content
 
+
 def eval_folder(gold_dir: Path, pred_dir: Path):
-    results = []
-    f1_scores = []
 
     for gold_file in gold_dir.iterdir():
         pred_file = pred_dir.joinpath(gold_file.name.replace("jsonl", "json.pred"))
@@ -37,18 +37,17 @@ def eval_folder(gold_dir: Path, pred_dir: Path):
             continue
         original_file = read_jsonlines(gold_file)
         pred = flatten([x["labels"] for x in read_jsonlines(pred_file)])
-        pred = [p.replace("_label", "") for p in pred ]
-        print(sorted(set(pred)))
-        print(gold_file, f1_score(original_file[0]["labels"][:len(pred)], pred, average=None, labels=sorted(set(pred))))
-        print(gold_file, f1_score(original_file[0]["labels"][:len(pred)], pred, average="weighted", labels=sorted(set(pred))))
+        pred = [p.replace("_label", "") for p in pred]
+        labels = sorted(set(original_file[0]["labels"][:len(pred)]))
+        print(gold_file, labels)
+        print(f1_score(original_file[0]["labels"][:len(pred)], pred, average=None, labels=labels))
+        print(f1_score(original_file[0]["labels"][:len(pred)], pred, average="weighted", labels=labels))
 
-
-    logging.info("Mean macro avg. f1 score over all files: %.2f" % np.mean(f1_scores))
+        print()
 
 if __name__ == '__main__':
     gold_dir = Path("data/tmp")
     pred_dir = Path("predictions")
-
 
     eval_folder(gold_dir=gold_dir, pred_dir=pred_dir)
 
