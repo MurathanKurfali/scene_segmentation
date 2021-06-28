44964b71025ecb7a29739bf146388f9a69991a3c
Murathan <murathankurfali@gmail.com>
2021-06-28 10:20:15 +0200

    label update

--- predict.py
+++ predict.py
@@ -1,8 +1,9 @@
+import glob
 import os
 
 import shutil
-from src.utils.preprocess import read_json
-from src.utils.postprocess import post_process
+from src_4label.utils.preprocess import read_json
+from src_4label.utils.postprocess import post_process
 import subprocess
 
 
@@ -16,24 +17,27 @@ if __name__ == "__main__":
     test_folder = "data/test"
     temp_folder = "data/tmp"
     pred_folder = "predictions"
-    model_file = "model/model.tar.gz"
+    listing = glob.glob('src_4label/large*')
 
-    test_files = sorted(os.listdir("data/test"))
-    reset_folder(temp_folder)
-    reset_folder(pred_folder)
+    for model_file in listing:
+        model_file = "{}/model.tar.gz".format(model_file)
+        print("**********", model_file)
+        test_files = sorted(os.listdir("data/test"))
+        reset_folder(temp_folder)
+        reset_folder(pred_folder)
 
-    for test_file in test_files:
-        if "9783845397535" not in test_file:
-            continue
-        test_file_path = "{}/{}".format(test_folder, test_file)
-        tmp_file_path = "{}/{}".format(temp_folder, test_file + "l")
-        predicted_file_path = "{}/{}".format(pred_folder, test_file + ".pred")
-        read_json(test_file_path, temp_folder, use_filename_as_split=True)
+        for test_file in test_files:
+            if "9783845397535" not in test_file:
+                continue
+            test_file_path = "{}/{}".format(test_folder, test_file)
+            tmp_file_path = "{}/{}".format(temp_folder, test_file + "l")
+            predicted_file_path = "{}/{}".format(pred_folder, test_file + ".pred")
+            read_json(test_file_path, temp_folder, use_filename_as_split=True)
 
-        subprocess.run('code/scripts/predict.sh {} {} {}'.format(model_file, tmp_file_path, predicted_file_path),
-                       shell=True)
-        print("post-processing")
-        post_process(test_file_path, predicted_file_path)
-        #os.remove(predicted_file_path)
-        print("done" + "#" * 15)
+            subprocess.run('src_4label/scripts/predict.sh {} {} {}'.format(model_file, tmp_file_path, predicted_file_path),
+                           shell=True)
+            print("post-processing")
+            post_process(test_file_path, predicted_file_path)
+            #os.remove(predicted_file_path)
+            print("done" + "#" * 15)
     # shutil.rmtree(temp_folder)
