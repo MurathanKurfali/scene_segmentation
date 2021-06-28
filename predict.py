886bf0b8e889da537112fe95c27c9e1b253738ba
Murathan <murathankurfali@gmail.com>
2021-06-28 13:48:00 +0200

    label update

--- predict.py
+++ predict.py
@@ -24,16 +24,15 @@ if __name__ == "__main__":
         model_file = "{}/model.tar.gz".format(model_file)
         print("**********", model_file)
         test_files = sorted(os.listdir("data/test"))
-        _pred_folder = pred_folder + "_" + model_file.split("/")[1]
         reset_folder(temp_folder)
-        reset_folder(_pred_folder)
+        reset_folder(pred_folder)
 
         for test_file in test_files:
             if True and "9783845397535" not in test_file:
                 continue
             test_file_path = "{}/{}".format(test_folder, test_file)
             tmp_file_path = "{}/{}".format(temp_folder, test_file + "l")
-            predicted_file_path = "{}/{}".format(_pred_folder, test_file + ".pred")
+            predicted_file_path = "{}/{}".format(pred_folder, test_file + ".pred")
             read_json(test_file_path, temp_folder, use_filename_as_split=True)  ## saves to data/tmp/
 
             subprocess.run('{}/scripts/predict.sh {} {} {}'.format(src, model_file, tmp_file_path, predicted_file_path),
