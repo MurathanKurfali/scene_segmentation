b65a2f0dc7d40f68c57db508588e1ea4e397c78a
Murathan <murathankurfali@gmail.com>
2021-07-06 16:23:25 +0200

    final touches

--- predict.py
+++ predict.py
@@ -18,7 +18,7 @@ if __name__ == "__main__":
     pred_folder = "predictions"
     model_file = "model.tar.gz"
     test_files = sorted(os.listdir("data/test"))
-
+    reset_folder(temp_folder)
     for test_file in test_files:
         test_file_path = "{}/{}".format(test_folder, test_file)
         tmp_file_path = "{}/{}".format(temp_folder, test_file + "l")
@@ -26,7 +26,7 @@ if __name__ == "__main__":
 
         read_json(test_file_path, temp_folder, use_filename_as_split=True)  ## saves to data/tmp/
 
-        subprocess.run('src/scripts/predict.sh {} {} {}'.format(model_file, tmp_file_path, predicted_file_path),
+        subprocess.run('code/scripts/predict.sh {} {} {}'.format(model_file, tmp_file_path, predicted_file_path),
                        shell=True)
         print("post-processing")
         post_process(test_file_path, tmp_file_path, predicted_file_path)
